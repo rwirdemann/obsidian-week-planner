6ebe8ba3afed7245b5c11b85261537d652b0acac
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2023-02-03 14:21:22 +0100

    Fixes test getWeekFileName

--- test/file.test.ts
+++ test/file.test.ts
@@ -1,6 +1,8 @@
 import {DATE_FORMAT} from "../src/date";
 import * as Moment from "moment";
 import {getWeekFileName} from "../src/file";
+import {WeekPlannerPluginSettings} from "../src/settings";
+import {WEEK_PLANNER_BASE_DIR, WEEK_PLANNER_DAYS_DIR, WEEK_WEEK_DIR} from "../src/constants";
 
 jest.mock('obsidian', () => ({
 	App: jest.fn().mockImplementation(),
@@ -8,9 +10,16 @@ jest.mock('obsidian', () => ({
 }));
 
 test('getWeekFileName', () => {
+	let settings: { workingDays: string, daysDir: string; weeksDir: string, baseDir: string } = {
+		workingDays: 'Mon,Tue,Wed,Thu,Fri',
+		baseDir: WEEK_PLANNER_BASE_DIR,
+		daysDir: WEEK_PLANNER_DAYS_DIR,
+		weeksDir: WEEK_WEEK_DIR
+	}
+
 	const sun = Moment("2022-10-23", DATE_FORMAT)
-	expect(getWeekFileName(sun)).toBe('Week Planner/Weeks/Calweek-2022-42.md');
+	expect(getWeekFileName(settings, sun)).toBe('Week Planner/Weeks/Calweek-2022-42.md');
 
 	const mon = Moment("2022-10-24", DATE_FORMAT)
-	expect(getWeekFileName(mon)).toBe('Week Planner/Weeks/Calweek-2022-43.md');
+	expect(getWeekFileName(settings, mon)).toBe('Week Planner/Weeks/Calweek-2022-43.md');
 });
