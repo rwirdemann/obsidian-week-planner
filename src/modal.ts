ed27dec68c1fde0d063060437b13397e30f0fe41
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-09-19 19:36:44 +0200

    Adds specific date to target list

--- src/modal.ts
+++ src/modal.ts
@@ -1,37 +1,46 @@
-import {App, DropdownComponent, Modal, Setting} from "obsidian";
+import {App, DropdownComponent, Modal, Setting, moment, Notice} from "obsidian";
 
 export class TodoModal extends Modal {
 	result: string;
 	targetList: string
+	targetDate: string
 
-	onSubmit: (result: string, targetList: string) => void;
+	onSubmit: (result: string, targetList: string, targetDate: Date) => void;
 
-	constructor(app: App, onSubmit: (result: string, targetList: string) => void) {
+	constructor(app: App, onSubmit: (result: string, targetList: string, targetDate: Date) => void) {
 		super(app);
 		this.onSubmit = onSubmit;
 		this.targetList = 'inbox'
+		this.targetDate = moment().format('YYYY-MM-DD');
 	}
 
 	onOpen() {
 		const {contentEl} = this;
 
 		new Setting(contentEl)
-			.setName("Decription")
+			.setName("Task Description")
 			.addText((text) =>
 				text.onChange((value) => {
 					this.result = value
 				})
-			);
-
-		new Setting(contentEl)
-			.setName("Target list")
+			)
 			.addDropdown(dropDown => {
 				dropDown.addOption('inbox', 'Inbox');
-				dropDown.addOption('today', 'Today');
+				dropDown.addOption('target-date', 'Specific date');
 				dropDown.onChange(async (value) => {
 					this.targetList = value
 				});
 			});
+
+		new Setting(contentEl)
+			.setName("Target date (only required if list' is 'Specific date')")
+			.addText(text => text
+				.setValue(this.targetDate)
+				.setPlaceholder('YYYY-MM-DD')
+				.onChange((value) => {
+					this.targetDate = value
+				})
+			);
 
 		new Setting(contentEl)
 			.addButton((btn) =>
@@ -39,13 +48,15 @@ export class TodoModal extends Modal {
 					.setButtonText("Submit")
 					.setCta()
 					.onClick(() => {
-						this.close();
-						this.onSubmit(this.result, this.targetList);
+						if (this.result.trim() != '' && (this.targetList == 'inbox' || isValidDate(this.targetDate))) {
+							this.close();
+							this.onSubmit(this.result, this.targetList, moment(this.targetDate).toDate());
+						}
 					}));
-	}
 
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
 	}
+}
+
+function isValidDate(date: string): boolean {
+	return moment(date, 'YYYY-MM-DD', true).isValid()
 }
