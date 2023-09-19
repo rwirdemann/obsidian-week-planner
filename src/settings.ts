cec3033a18743a33980e1baeff19cb296a50e79c
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-09-19 19:36:46 +0200

    Adds week directory to settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,13 +1,15 @@
-import {WEEK_PLANNER_BASE_DIR, WEEK_PLANNER_DAYS_DIR} from "./constants";
+import {WEEK_PLANNER_BASE_DIR, WEEK_PLANNER_DAYS_DIR, WEEK_WEEK_DIR} from "./constants";
 
 export interface WeekPlannerPluginSettings {
 	workingDays: string;
 	baseDir: string
 	daysDir: string
+	weeksDir: string
 }
 
 export const DEFAULT_SETTINGS: WeekPlannerPluginSettings = {
 	workingDays: 'Mon,Tue,Wed,Thu,Fri',
 	baseDir: WEEK_PLANNER_BASE_DIR,
-	daysDir: WEEK_PLANNER_DAYS_DIR
+	daysDir: WEEK_PLANNER_DAYS_DIR,
+	weeksDir: WEEK_WEEK_DIR
 }
